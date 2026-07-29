0e4bbb6d997bf26fcd23f2aec77bc955e0b225ee
user.email <c20cabonilasc@gmail.com>
2026-07-29 17:43:16 -0400

    AuthenticationDAO completed.

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/AuthenticationDAO.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/AuthenticationDAO.java
@@ -1,4 +1,32 @@
 package com.rev.ers.repo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AuthenticationDAO {
+    private final Connection connection;
+
+    public AuthenticationDAO(Connection connection){
+        this.connection = connection;
+    }
+
+    public boolean authenticate(String username, String password){
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try(PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            try (ResultSet result = statement.executeQuery()){
+                return result.next();
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
