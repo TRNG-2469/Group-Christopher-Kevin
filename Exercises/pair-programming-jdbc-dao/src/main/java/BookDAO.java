9de13a2cd8d723a91dc4134047885fc62f1fe43c
vintaing <vintaing@gmail.com>
2026-07-30 16:46:39 -0500

    added empty project for pair-programming-jdbc-dao exercise and completed task 1

---
+++ Exercises/pair-programming-jdbc-dao/src/main/java/BookDAO.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface BookDAO {
+    void save(Book book);
+    Book getById(int id);
+    List<Book> getAll();
+    void update(Book book);
+    void delete(int id);
+}
