e7ee2327d2a51e9d9fd55f99fc65c5545ffe96a4
user.email <c20cabonilasc@gmail.com>
2026-07-31 15:44:46 -0400

    Refactored authenticate method

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/ReimbursementDAO.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/ReimbursementDAO.java
@@ -1,4 +1,16 @@
 package com.rev.ers.repo;
 
+import com.rev.ers.model.Reimbursement;
+
+import java.util.List;
+
 public interface ReimbursementDAO {
+    // CRUD operations
+    void create(Reimbursement employee);
+    void update(Reimbursement employee);
+    void delete(int id);
+
+    // Read All, Read One
+    Reimbursement findByID(int id);
+    List<Reimbursement> findAll();
 }
