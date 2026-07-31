2ea23c26b1b7544ee98a889ada4458f19b80eb10
vintaing <vintaing@gmail.com>
2026-07-31 14:22:43 -0500

    updated Role enum to include string value

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/enums/Role.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/enums/Role.java
@@ -1,6 +1,19 @@
 package com.rev.ers.enums;
 
 public enum Role {
-    EMPLOYEE,
-    MANAGER
+    EMPLOYEE("employee"),
+    MANAGER("manager");
+
+    private final String dbValue;
+
+    Role(String dbValue) {
+        this.dbValue = dbValue;
+    }
+
+    public String getDbValue() {
+        return dbValue;
+    }
+
+    // For Christopher
+    // stmt.setString(2, user.getRole().getDbValue());
 }
