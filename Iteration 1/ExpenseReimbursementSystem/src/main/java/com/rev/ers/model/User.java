ea33f7bef3fff4b72002a80bde822b18bed96df7
vintaing <vintaing@gmail.com>
2026-07-31 14:13:09 -0500

    added working abstract class User

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/User.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/User.java
@@ -1,4 +1,81 @@
 package com.rev.ers.model;
 
-public class User {
+import com.rev.ers.enums.Role;
+
+public abstract class User {
+    private int user_id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Role role;
+    private int department_id;
+
+    private User() {}
+
+    public User(int user_id, String username, String password, String firstName, String lastName, Role role, int department_id) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+        this.department_id = department_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
 }
