beccf2d9b22797460cecba53153344e5d4ea0539
vintaing <vintaing@gmail.com>
2026-07-31 14:37:44 -0500

    added constructors for Manager.java

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/Manager.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/Manager.java
@@ -1,4 +1,13 @@
 package com.rev.ers.model;
 
-public class Manager {
+import com.rev.ers.enums.Role;
+
+public class Manager extends User{
+    private Manager() {
+        super(0, "", "", "", "", Role.MANAGER, 0);
+    }
+
+    public Manager(int user_id, String username, String password, String firstName, String lastName, int department_id) {
+        super(user_id, username, password, firstName, lastName, Role.MANAGER, department_id);
+    }
 }
