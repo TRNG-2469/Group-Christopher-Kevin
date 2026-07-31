0dd701d8e7d73551427386d8bee8f364cc13b195
vintaing <vintaing@gmail.com>
2026-07-31 14:35:17 -0500

    added constructors for Employee.java

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/Employee.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/Employee.java
@@ -1,4 +1,13 @@
 package com.rev.ers.model;
 
-public class Employee {
+import com.rev.ers.enums.Role;
+
+public class Employee extends User{
+    private Employee() {
+        super(0, "", "", "", "", Role.EMPLOYEE, 0);
+    }
+
+    public Employee(int user_id, String username, String password, String firstName, String lastName, int department_id) {
+        super(user_id, username, password, firstName, lastName, Role.EMPLOYEE, department_id);
+    }
 }
