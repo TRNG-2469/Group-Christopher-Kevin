b6ac458198f2ddba7831d4d45ac8e2c60b49a8ff
vintaing <vintaing@gmail.com>
2026-08-03 16:23:16 -0500

    added instantiation of classes with constructor injection into app.java

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/App.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/App.java
@@ -1,10 +1,22 @@
 package com.rev.ers;
 
+import com.rev.ers.controller.*;
+import com.rev.ers.repo.*;
+import com.rev.ers.service.*;
+
 import io.javalin.Javalin;
 
 public class App {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
         app.get("/", ctx -> ctx.result("Hello World"));
+
+        UserDAO userDAO = new UserDAOImp();
+        UserService userService = new UserServiceImp(userDAO);
+        UserHandler userHandler = new UserHandlerImp(userService);
+
+        ReimbursementDAO ReimbursementDAO = new ReimbursementDAOImp();
+        ReimbursementService ReimbursementService = new ReimbursementServiceImp(ReimbursementDAO);
+        ReimbursementHandler ReimbursementHandler = new ReimbursementHandlerImp(ReimbursementService);
     }
 }
