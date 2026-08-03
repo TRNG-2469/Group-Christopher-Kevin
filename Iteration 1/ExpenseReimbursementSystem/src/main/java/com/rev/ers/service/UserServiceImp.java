f1e32a7d1dde506bbd06b0a512934d555217dc53
user.email <c20cabonilasc@gmail.com>
2026-08-03 16:31:31 -0400

    Completed authenticate method

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/UserServiceImp.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/UserServiceImp.java
@@ -1,11 +1,18 @@
 package com.rev.ers.service;
 
 import com.rev.ers.model.User;
+import com.rev.ers.repo.UserDAO;
+import com.rev.ers.repo.UserDAOImp;
 
 public class UserServiceImp implements UserService{
     @Override
     public User authenticate(String username, String password) {
-        return null;
+        UserDAO DAO = new UserDAOImp();
+        if(username == null || password == null || username.isEmpty() || password.isEmpty()){
+            throw new IllegalArgumentException("Username or password cannot be null or empty.");
+        }
+
+        return DAO.authenticate(username, password);
     }
 
     @Override
