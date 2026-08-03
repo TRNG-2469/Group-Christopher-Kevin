43e587eafa1bd6bd72310e1ff5a9c49f04a3a14a
user.email <c20cabonilasc@gmail.com>
2026-08-03 16:28:14 -0400

    Implemented skeleton classes for UserServiceImp

---
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/UserServiceImp.java
@@ -0,0 +1,15 @@
+package com.rev.ers.service;
+
+import com.rev.ers.model.User;
+
+public class UserServiceImp implements UserService{
+    @Override
+    public User authenticate(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public void register(User user) {
+
+    }
+}
