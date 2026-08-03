faffc1ccc12fd8cfc675ed363a88136ff55cbec4
user.email <c20cabonilasc@gmail.com>
2026-08-03 16:40:16 -0400

    Implemented empty methods to reimbursementserviceimp

---
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/ReimbursementServiceImp.java
@@ -0,0 +1,26 @@
+package com.rev.ers.service;
+
+import com.rev.ers.model.Reimbursement;
+import java.util.List;
+
+public class ReimbursementServiceImp implements ReimbursementService{
+    @Override
+    public void create(Reimbursement reimbursement) {
+
+    }
+
+    @Override
+    public void update(Reimbursement reimbursement) {
+
+    }
+
+    @Override
+    public Reimbursement findByAuthor(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Reimbursement> findAll() {
+        return List.of();
+    }
+}
