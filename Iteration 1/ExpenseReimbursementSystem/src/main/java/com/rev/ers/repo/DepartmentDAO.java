1f2d77888ac1b44a80c791fd8a1bae82ff3d44a2
vintaing <vintaing@gmail.com>
2026-08-07 15:44:03 -0500

    added departmentdao interface

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/DepartmentDAO.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/DepartmentDAO.java
@@ -1,4 +1,10 @@
 package com.rev.ers.repo;
 
+import com.rev.ers.model.Department;
+
+import java.util.List;
+
 public interface DepartmentDAO {
+    Department findDepartmentById(int id);
+    List<Department> findAll();
 }
