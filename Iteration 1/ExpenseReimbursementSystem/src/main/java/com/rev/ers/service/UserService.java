5325f56787a1ba2e7903e4d0ab82b2962253992b
vintaing <vintaing@gmail.com>
2026-08-07 16:47:07 -0500

    added searchby username implementation and interface in service layer

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/UserService.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/UserService.java
@@ -3,6 +3,7 @@ package com.rev.ers.service;
 import com.rev.ers.model.User;
 
 public interface UserService {
+    public User searchByUsername(String username);
     public User authenticate(String username, String password);
     public User register(User user);
 }
--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/UserServiceImp.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/UserServiceImp.java
@@ -1,37 +1,55 @@
 package com.rev.ers.service;
 
 import com.rev.ers.model.User;
+import com.rev.ers.repo.DepartmentDAO;
 import com.rev.ers.repo.UserDAO;
 import com.rev.ers.repo.UserDAOImp;
 
 public class UserServiceImp implements UserService{
     private final UserDAO userDAO;
+    private final DepartmentDAO departmentDAO;
 
-    public UserServiceImp(UserDAO userDAO) {
+    public UserServiceImp(UserDAO userDAO, DepartmentDAO departmentDAO) {
         this.userDAO = userDAO;
+        this.departmentDAO = departmentDAO;
+    }
+
+    @Override
+    public User searchByUsername(String username) {
+        // Ensures username input is valid
+        if(username == null || username.isEmpty()){
+            throw new IllegalArgumentException("Username cannot be null or empty.");
+        }
+        return userDAO.searchByUsername(username);
     }
 
     @Override
     public User authenticate(String username, String password) {
+        // Ensures username and password inputs are valid
         if(username == null || password == null || username.isEmpty() || password.isEmpty()){
             throw new IllegalArgumentException("Username or password cannot be null or empty.");
         }
-
         return userDAO.authenticate(username, password);
     }
 
     @Override
     public User register(User user) {
-        if(user.getUsername() == null || user.getUsername().isEmpty()
-                || user.getPassword() == null || user.getPassword().isEmpty()){
+        // Ensures username does not already exist and username input is valid
+        if(searchByUsername(user.getUsername()) != null){
+            throw new IllegalArgumentException("Username already exists.");
+        }
+        // Ensures password is not null or empty
+        if(user.getPassword() == null || user.getPassword().isEmpty()){
             throw new IllegalArgumentException("Username or password cannot be null or empty.");
-
-        } else if(user.getFirstName() == null || user.getFirstName().isEmpty()
+        }
+        // Ensures first and last name are not null or empty
+        if(user.getFirstName() == null || user.getFirstName().isEmpty()
                 || user.getLastName() == null || user.getLastName().isEmpty()){
             throw new IllegalArgumentException("First or last name cannot be null or empty.");
-
-        } else if(user.getDepartment_id() > 0){
-            throw new IllegalArgumentException("Department ID cannot be negative.");
+        }
+        // Ensures department ID exists
+        if(departmentDAO.findDepartmentById(user.getDepartment_id()) == null){
+            throw new IllegalArgumentException("Department ID does not exist.");
         }
 
         return userDAO.register(user);
