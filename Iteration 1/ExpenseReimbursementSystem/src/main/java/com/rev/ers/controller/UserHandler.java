06dd7e94777c1137382d78f2687d2435be76284c
vintaing <vintaing@gmail.com>
2026-08-07 15:35:12 -0500

    added implementation to UserService

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/UserHandler.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/UserHandler.java
@@ -1,4 +1,11 @@
 package com.rev.ers.controller;
 
+import com.rev.ers.model.User;
+import io.javalin.http.Context;
+
 public interface UserHandler {
+
+    public void authenticate(Context ctx);
+
+    public void register(Context ctx);
 }
--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/UserHandlerImp.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/UserHandlerImp.java
@@ -1,11 +1,36 @@
 package com.rev.ers.controller;
 
+import com.rev.ers.model.User;
 import com.rev.ers.service.UserService;
+import io.javalin.http.Context;
 
 public class UserHandlerImp implements UserHandler{
     private final UserService userService;
 
     public UserHandlerImp(UserService userService) {
         this.userService = userService;
+    }
+
+
+    @Override
+    public void authenticate(Context ctx) {
+        User user = ctx.bodyAsClass(User.class);
+        User authenticatedUser = userService.authenticate(user.getUsername(), user.getPassword());
+        if (authenticatedUser != null) {
+            ctx.status(200).json(authenticatedUser);
+        } else {
+            ctx.status(401).result("Invalid username or password.");
+        }
+    }
+
+    @Override
+    public void register(Context ctx) {
+        User user = ctx.bodyAsClass(User.class);
+        User registeredUser = userService.register(user);
+        if (registeredUser != null) {
+            ctx.status(201).json(registeredUser);
+        } else {
+            ctx.status(400).result("Failed to register user.");
+        }
     }
 }
