a02e8ed7811671e0cfe293c8ec2a16f82a085b82
vintaing <vintaing@gmail.com>
2026-08-07 16:45:39 -0500

    added searchbyusername method in userdao interface

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/UserDAO.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/repo/UserDAO.java
@@ -3,6 +3,7 @@ package com.rev.ers.repo;
 import com.rev.ers.model.User;
 
 public interface UserDAO {
+    public User searchByUsername(String username);
     public User authenticate(String username, String password);
     public User register(User user);
 }
