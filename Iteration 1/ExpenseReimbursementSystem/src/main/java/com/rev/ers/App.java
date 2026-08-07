b577a7657b640b6cc3d1a340916f577b145e2dff
vintaing <vintaing@gmail.com>
2026-08-07 16:45:12 -0500

    added new endpoints in app.java

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/App.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/App.java
@@ -9,14 +9,22 @@ import io.javalin.Javalin;
 public class App {
     public static void main(String[] args) {
         UserDAO userDAO = new UserDAOImp();
-        UserService userService = new UserServiceImp(userDAO);
-        UserHandler userHandler = new UserHandlerImp(userService);
-
+        DepartmentDAO departmentDAO = new DepartmentDAOImp();
         ReimbursementDAO reimbursementDAO = new ReimbursementDAOImp();
+
+        UserService userService = new UserServiceImp(userDAO, departmentDAO);
+        DepartmentService departmentService = new DepartmentServiceImp(departmentDAO);
         ReimbursementService reimbursementService = new ReimbursementServiceImp(reimbursementDAO);
+
+        UserHandler userHandler = new UserHandlerImp(userService);
+        DepartmentHandler departmentHandler = new DepartmentHandlerImp(departmentService);
         ReimbursementHandler reimbursementHandler = new ReimbursementHandlerImp(reimbursementService);
 
         Javalin app = Javalin.create().start(7000);
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.post("/register", userHandler::register);
+        app.post("/login", userHandler::authenticate);
+        app.get("/departments/{id}", departmentHandler::findDepartmentById);
+        app.get("/departments", departmentHandler::findAll);
     }
 }
