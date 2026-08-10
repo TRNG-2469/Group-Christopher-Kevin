de853147cc2f4afd06c5e43180e4ab0cb9523b08
vintaing <vintaing@gmail.com>
2026-08-10 14:35:13 -0500

    reduced redundancy in reimbursementservice by implementing a validation helper class

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/ReimbursementServiceImp.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/service/ReimbursementServiceImp.java
@@ -13,33 +13,13 @@ public class ReimbursementServiceImp implements ReimbursementService{
 
     @Override
     public void create(Reimbursement reimbursement) {
-        if(reimbursement.getReimbursement_id() <= 0){
-            throw new IllegalArgumentException("Reimbursement ID cannot be negative or zero.");
-        } else if(reimbursement.getAmount() <= 0.0){
-            throw new IllegalArgumentException("Reimbursement amount cannot be negative or zero.");
-        } else if(reimbursement.getDescription() == null || reimbursement.getDescription().isBlank()){
-            throw new IllegalArgumentException("Description cannot be null or blank.");
-        } else if(reimbursement.getType() == null || reimbursement.getStatus() == null){
-            throw new IllegalArgumentException("Type and status cannot be null.");
-        } else if(reimbursement.getAuthor_id() <= 0 || reimbursement.getResolver_id() <= 0){
-            throw new IllegalArgumentException("Author or Resolver ID cannot be negative or zero.");
-        }
+        validation(reimbursement);
         reimbursementDAO.create(reimbursement);
     }
 
     @Override
     public void update(Reimbursement reimbursement) {
-        if(reimbursement.getReimbursement_id() <= 0){
-            throw new IllegalArgumentException("Reimbursement ID cannot be negative or zero.");
-        } else if(reimbursement.getAmount() <= 0.0){
-            throw new IllegalArgumentException("Reimbursement amount cannot be negative or zero.");
-        } else if(reimbursement.getDescription() == null || reimbursement.getDescription().isBlank()){
-            throw new IllegalArgumentException("Description cannot be null or blank.");
-        } else if(reimbursement.getType() == null || reimbursement.getStatus() == null){
-            throw new IllegalArgumentException("Type and status cannot be null.");
-        } else if(reimbursement.getAuthor_id() <= 0 || reimbursement.getResolver_id() <= 0){
-            throw new IllegalArgumentException("Author or Resolver ID cannot be negative or zero.");
-        }
+        validation(reimbursement);
         reimbursementDAO.update(reimbursement);
     }
 
@@ -54,5 +34,20 @@ public class ReimbursementServiceImp implements ReimbursementService{
     @Override
     public List<Reimbursement> findAll() {
         return reimbursementDAO.findAll();
+    }
+
+    private boolean validation(Reimbursement reimbursement) {
+        if(reimbursement.getReimbursement_id() <= 0){
+            throw new IllegalArgumentException("Reimbursement ID cannot be negative or zero.");
+        } else if(reimbursement.getAmount() <= 0.0){
+            throw new IllegalArgumentException("Reimbursement amount cannot be negative or zero.");
+        } else if(reimbursement.getDescription() == null || reimbursement.getDescription().isBlank()){
+            throw new IllegalArgumentException("Description cannot be null or blank.");
+        } else if(reimbursement.getType() == null || reimbursement.getStatus() == null){
+            throw new IllegalArgumentException("Type and status cannot be null.");
+        } else if(reimbursement.getAuthor_id() <= 0 || reimbursement.getResolver_id() <= 0){
+            throw new IllegalArgumentException("Author or Resolver ID cannot be negative or zero.");
+        }
+        return true;
     }
 }
