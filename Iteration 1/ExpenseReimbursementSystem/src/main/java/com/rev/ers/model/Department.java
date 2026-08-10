96cea82cc3a41da590a3e211aa5ad21dc1192ae0
vintaing <vintaing@gmail.com>
2026-08-10 14:38:57 -0500

    removed setter methods to ensure encapsulation thru immutability

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/Department.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/model/Department.java
@@ -21,14 +21,6 @@ public class Department {
         return name;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String toString() {
         return "Department{" +
