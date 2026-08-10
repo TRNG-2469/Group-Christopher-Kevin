e0485f5a79f95449f35c7b319e3ce6152688c6b0
vintaing <vintaing@gmail.com>
2026-08-10 09:45:40 -0500

    added reimbursementhandler implementation

--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/ReimbursementHandler.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/ReimbursementHandler.java
@@ -1,4 +1,11 @@
 package com.rev.ers.controller;
 
+import io.javalin.http.Context;
+
 public interface ReimbursementHandler {
+    void create(Context ctx);
+    void update(Context ctx);
+
+    void findByAuthor(Context ctx);
+    void findAll(Context ctx);
 }
--- Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/ReimbursementHandlerImp.java
+++ Iteration 1/ExpenseReimbursementSystem/src/main/java/com/rev/ers/controller/ReimbursementHandlerImp.java
@@ -1,11 +1,50 @@
 package com.rev.ers.controller;
 
+import com.rev.ers.model.Reimbursement;
 import com.rev.ers.service.ReimbursementService;
+import io.javalin.http.Context;
+
+import java.util.List;
 
 public class ReimbursementHandlerImp implements ReimbursementHandler {
     private final ReimbursementService reimbursementService;
 
     public ReimbursementHandlerImp(ReimbursementService reimbursementService) {
         this.reimbursementService = reimbursementService;
+    }
+
+    @Override
+    public void create(Context ctx) {
+        Reimbursement reimbursement = ctx.bodyAsClass(Reimbursement.class);
+        reimbursementService.create(reimbursement);
+        ctx.status(201).result("Reimbursement created successfully.");
+    }
+
+    @Override
+    public void update(Context ctx) {
+        Reimbursement reimbursement = ctx.bodyAsClass(Reimbursement.class);
+        reimbursementService.update(reimbursement);
+        ctx.status(200).result("Reimbursement updated successfully.");
+    }
+
+    @Override
+    public void findByAuthor(Context ctx) {
+        int id = Integer.parseInt(ctx.pathParam("id"));
+        Reimbursement reimbursement = reimbursementService.findByAuthor(id);
+        if (reimbursement != null) {
+            ctx.status(200).json(reimbursement);
+        } else {
+            ctx.status(404).result("Reimbursement not found.");
+        }
+    }
+
+    @Override
+    public void findAll(Context ctx) {
+        List<Reimbursement> reimbursements = reimbursementService.findAll();
+        if (!reimbursements.isEmpty()) {
+            ctx.status(200).json(reimbursements);
+        } else {
+            ctx.status(404).result("No reimbursements found.");
+        }
     }
 }
